4d08233a9fff91ae319351cf2cf5536708e49473
kravtsovyehor <kravtsovyehor@gmail.com>
2026-07-15 22:44:21 +0200

    build(testing): add jest plugin to ESLint configuration and update dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,24 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import jestPlugin from 'eslint-plugin-jest';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    files: ['**/*.{test,spec}.{js,jsx,ts,tsx}'],
+    ...jestPlugin.configs['flat/recommended'],
+    rules: {
+      ...jestPlugin.configs['flat/recommended'].rules,
+      'jest/prefer-expect-assertions': 'off',
+    },
+  },
+  // you can also configure jest rules in other objects, so long as some of the `files` match
+  {
+    files: ['**/*.{test,spec}.{js,jsx,ts,tsx}'],
+    rules: { 'jest/prefer-expect-assertions': 'off' },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
@@ -12,6 +26,7 @@ const eslintConfig = defineConfig([
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "coverage/**",
   ]),
 ]);
 
