eaa5acdd2a6cb707ade775b2e5263d80818c3a3a
kravtsovyehor <kravtsovyehor@gmail.com>
2026-07-29 17:17:44 +0200

    Fix color contrast accessibility violation

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,11 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ['../src/components/**/*.stories.@(ts|tsx)'],
   staticDirs: ["../public"],
-  addons: ["@storybook/addon-docs", "@storybook/addon-vitest"],
+  addons: [
+    "@storybook/addon-docs",
+    "@storybook/addon-vitest",
+    "@storybook/addon-a11y"
+  ],
   framework: {
     name: "@storybook/react-vite",
     options: {},
