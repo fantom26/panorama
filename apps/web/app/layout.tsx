19c8193c3476eb3fde2155a46fcfdc89e5ff281b
kravtsovyehor <kravtsovyehor@gmail.com>
2026-08-04 18:54:44 +0200

    refactor: remove unused font files

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -13,8 +13,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang='en' className='h-full antialiased' data-theme='dark'>
-      <body className='min-h-full flex flex-col'>{children}</body>
+    <html lang='en' data-theme='dark'>
+      <body>{children}</body>
     </html>
   )
 }
