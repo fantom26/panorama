6e4e556b42c90d79bbc5e3536652416110307eda
kravtsovyehor <kravtsovyehor@gmail.com>
2026-08-12 18:56:55 +0200

    feat(Select): add Select component with styles and stories

--- packages/ui/src/components/Icon/icons.ts
+++ packages/ui/src/components/Icon/icons.ts
@@ -1,7 +1,9 @@
-import { ArrowLeft, CircleAlert, Inbox, Plus, Search, X } from 'lucide-react'
+import { ArrowLeft, Check, ChevronDown, CircleAlert, Inbox, Plus, Search, X } from 'lucide-react'
 
 export const icons = {
   'arrow-left': ArrowLeft,
+  check: Check,
+  'chevron-down': ChevronDown,
   'circle-alert': CircleAlert,
   inbox: Inbox,
   plus: Plus,
